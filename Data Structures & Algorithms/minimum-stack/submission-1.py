f99243fc86b1e267655ccf8f8dbda1b4e3643be3
Wanjun Lan <59032164+wjlan@users.noreply.github.com>
2026-07-15 16:28:00 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        min_num = min(self.stack)
+        return min_num
+
+        
